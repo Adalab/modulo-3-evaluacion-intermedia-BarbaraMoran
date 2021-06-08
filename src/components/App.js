d5e67578365c6bbf6b2263ac83bca278b963e7df
BarbaraMoran <barbaramoranrr@gmail.com>
2021-06-08 11:05:52 +0200

    add state

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,14 @@
-import React from "react";
+import { useState } from "react";
 import "../stylesheets/App.scss";
 import PokeList from "./PokeList";
 import pokemonData from "../data/pokemonData.json";
-//import { useState } from "react";
 
-const App = () => {
-  //const [pokemonData, setPokemonData] = useState([pokemonData]);
+const App = (props) => {
+  const [pokemons, setPokemonData] = useState(pokemonData);
   return (
     <div className="App">
       <h1 className="title"> Mi lista de Pokemon</h1>
-      <PokeList pokemonData={pokemonData} />
+      <PokeList pokemonData={pokemons} />
     </div>
   );
 };
